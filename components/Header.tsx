57eccad3d8a2547dc9b40a79d21f68dbbfbe74a8
aIacoella <angeloiacoella@gmail.com>
2020-03-03 21:36:21 +0100

    Resolve merge conflicts changes #1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,100 +1,90 @@
-import React, {useState, FunctionComponent} from "react";
-import { default as NextLink } from "next/link";
-import Link from "../components/Link";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 import * as colors from "../utils/colors";
 import Button from "./Button";
-import ReactModal from "react-modal";
-import { Category } from "../utils/types";
-import ModalComponent from "./ModalComponent";
 
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState<string | number>("100vw");
 
-const Header: FunctionComponent<{}> = () => {
-  const [modalOpen, setModal] = useState(false);
+  const updateWindowWidth = () =>
+    setWindowWidth(
+      document.getElementsByTagName("body")[0].getBoundingClientRect().width
+    );
 
-  const openModal = () => {
-    setModal(true);
-  }
+  const getWindowWidth = () =>
+    typeof windowWidth === "string" ? windowWidth : windowWidth + "px";
 
-  const closeModal = () => {
-    setModal(false);
-  }
+  useEffect(() => {
+    updateWindowWidth();
+    window.addEventListener("resize", updateWindowWidth);
+    return () => window.removeEventListener("resize", updateWindowWidth);
+  }, []);
 
   return (
     <div className="header">
-      <NextLink href="/">
+      <Link href="/">
         <a className="uppercase header-title">
           <img alt="RNS" src="/media/Logo.svg" />
           react native stack
         </a>
-      </NextLink>
-      <div className="main-inner-header">
-        <Link
-          content="home"
-          color={colors.BLACK}
-          href="/"
-          size="l"
-          style={{ margin: "0px 40px 0px 40px" }}
-        />
-        <Link
-          content="about"
-          color={colors.BLACK}
-          href="/about"
-          size="l"
-          style={{ margin: "0px 40px 0px 40px" }}
-        />
-        <Link
-          content="contributors"
-          color={colors.BLACK}
-          href="/contributors"
-          size="l"
-          style={{ margin: "0px 40px 0px 40px" }}
-        />
+      </Link>
+      <button
+        onClick={() => setIsOpen(true)}
+        style={{ padding: "10px" }}
+        className="open-menu"
+      >
+        <img src="/media/menu.svg" alt="MENU" />
+      </button>
+      <div className={"main-inner-header " + (isOpen && "open")}>
+        <Link href="/">
+          <a className="link">home</a>
+        </Link>
+        <Link href="/about">
+          <a className="link">about</a>
+        </Link>
+        <Link href="/contributions">
+          <a className="link">contributions</a>
+        </Link>
+        <div className="mobile-contibution-btn">
+          <Button>Add Resource</Button>
+        </div>
       </div>
 
       <div className="contributes-button">
-        <Button onClick={openModal}>Contribute</Button>
+        <Button>Add Resource</Button>
       </div>
-
-      <ReactModal isOpen = {modalOpen} shouldCloseOnEsc = {true} onRequestClose={closeModal} className="contribute-modal">
-        <ModalComponent />
-      </ReactModal>
-
+      <div className={"mobile-overlay " + (isOpen && "open")}>
+        <button className="exit-header" onClick={() => setIsOpen(false)}>
+          <img src="/media/exit.svg" alt="X" />
+        </button>
+      </div>
+      <button className={"add-resource-mobile " + (isOpen && "open")}>
+        <img src="/media/addResource.svg" alt="ADD RESOURCE" />
+      </button>
       <style jsx global>{`
-
-        .contribute-modal {
-          outline: none;
-          width:40%;
-          padding: 1px;
-          border: 1px solid ${colors.BLACK};
-          margin: 10% auto;
-          background-color: ${colors.WHITE};
-          border-radius: 3px;
-          height: 40%;
-          display: flex;
-          flex-direction: row;
-        }
-
         .header {
+          max-width: 100%;
           width: 100vw;
-          padding: 30px 80px;
+          height: 100px;
+          padding: 0 50px;
           display: flex;
           flex-direction: row;
           align-items: center;
-          justify-content: center;
           box-sizing: border-box;
+          justify-content: space-between;
         }
-
         .header-title {
           box-sizing: border-box;
-          flex: 1;
           font-size: 24px;
           font-weight: 400;
           letter-spacing: 0.1em;
-          color: ${colors.BLACK};
+          color: ${colors.PRIMARY_BLUE};
           box-sizing: border-box;
           text-decoration: none;
           display: flex;
           align-items: center;
+          width: 350px;
         }
         .header-title > img {
           margin-right: 14px;
@@ -102,7 +92,6 @@ const Header: FunctionComponent<{}> = () => {
 
         .main-inner-header {
           box-sizing: border-box;
-          flex: 1;
           display: flex;
           justify-content: center;
           flex-direction: row;
@@ -111,13 +100,138 @@ const Header: FunctionComponent<{}> = () => {
 
         .contributes-button {
           box-sizing: border-box;
-          flex: 1;
           display: flex;
           justify-content: flex-end;
+          width: 350px;
+        }
+
+        .link {
+          width: 200px;
+          text-align: center;
+          text-transform: uppercase;
+          letter-spacing: 0.1em;
+          font-size: 18px;
+          font-weight: 400;
+          color: ${colors.BLACK};
+          text-decoration: none;
+        }
+        .link:hover {
+          cursor: pointer;
+          text-decoration: underline;
+        }
+
+        .mobile-contibution-btn {
+          display: none;
+          flex: 1;
+          flex-direction: column;
+          justify-content: flex-end;
+          padding-bottom: 6px;
+        }
+        .open-menu {
+          display: none;
+        }
+
+        .mobile-overlay {
+          display: none;
+        }
+        .add-resource-mobile {
+          display: none;
+        }
+
+        @media screen and (max-width: 1000px) {
+          .header {
+            padding: 0 10px;
+            height: 60px;
+            border-bottom: solid 2px ${colors.PRIMARY_BLUE};
+          }
+          .header-title {
+            font-size: 1.34em;
+          }
+          .contributes-button {
+            display: none;
+          }
+          .main-inner-header {
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            z-index: 999;
+            width: calc(${getWindowWidth()} - ${DRAWER_MARGIN}px);
+            background-color: ${colors.WHITE};
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: flex-start;
+            padding: 60px 20px 20px 20px;
+            transform: translateX(
+              calc(${getWindowWidth()} - ${DRAWER_MARGIN}px)
+            );
+            transition: 0.3s;
+          }
+          .main-inner-header.open {
+            transform: translateX(0);
+          }
+          .mobile-overlay {
+            position: fixed;
+            display: block;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            background-color: rgba(0, 0, 0, 0.8);
+            z-index: 998;
+            opacity: 0;
+            transition: 0.3s;
+            pointer-events: none;
+          }
+          .mobile-overlay.open {
+            pointer-events: auto;
+            opacity: 1;
+          }
+          .link {
+            text-align: left;
+            padding: 20px 0;
+            width: 100%;
+          }
+          .mobile-contibution-btn {
+            display: flex;
+          }
+          .open-menu {
+            display: block;
+          }
+          .exit-header {
+            position: absolute;
+            left: ${(DRAWER_MARGIN - 40) / 2}px;
+            top: 20px;
+            width: 40px;
+            background-color: transparent;
+            border: none;
+            padding: 0;
+            outline: none;
+          }
+          .add-resource-mobile {
+            z-index: 999;
+            position: fixed;
+            display: block;
+            right: 20px;
+            bottom: 20px;
+            border: none;
+            background-color: transparent;
+            outline: none;
+            transition: 0.3s;
+          }
+          .add-resource-mobile.open {
+            transform: translateX(
+              calc(
+                ${DRAWER_MARGIN}px - ${getWindowWidth()} + 20px -
+                  ${(DRAWER_MARGIN - ADD_RESOURCE_BTN_WIDTH) / 2}px
+              )
+            );
+          }
         }
       `}</style>
     </div>
   );
 }
 
-export default Header;
+const DRAWER_MARGIN = 70;
+const ADD_RESOURCE_BTN_WIDTH = 50;
--- layouts/SearchLayout.tsx
+++ layouts/SearchLayout.tsx
@@ -4,54 +4,44 @@ import * as colors from "../utils/colors";
 interface SearchLayoutProps {
   callback: (e: React.ChangeEvent<HTMLInputElement>) => void;
   keyword: string;
+  placeholder: string;
 }
 
 const SearchLayout: FunctionComponent<SearchLayoutProps> = ({
   children,
   callback,
-  keyword
+  keyword,
+  placeholder
 }) => {
   return (
-    <div>
+    <div className="content-container">
       <div className="search-div">
-        <form>
-          <input
-            type="text"
-            name="filter"
-            className="search-field"
-            placeholder="Filter here..."
-            value={keyword}
-            onChange={callback}
-          />
-        </form>
+        <input
+          type="text"
+          name="filter"
+          className="search-field"
+          placeholder={placeholder}
+          value={keyword}
+          onChange={callback}
+        />
         {children}
       </div>
       <style jsx>
         {`
-            .search-div{
-                display: :flex;
-                flex-direction: column;
-                justify-content: center;
-                witdh: 100%;
-            }
-
-            .search-div > form{
-                width:100%;
-                display: flex;
-                flex-direction: row;
-                justify-content: flex-start;
-                margin-bottom: 50px;
-            }
-
-            .search-field{
-                font-size: 18px;
-                width:100%;
-                border-radius:5px;
-                height: 40px;
-                border: 1px solid ${colors.BLACK};
-                padding-left: 2%;
-            }
-          `}
+          .content-container {
+            max-width: 800px;
+            width: calc(100vw - 30px);
+          }
+          .search-field {
+            width: 100%;
+            font-size: 18px;
+            border-radius: 5px;
+            height: 40px;
+            border: 1px solid ${colors.BLACK};
+            padding-left: 15px;
+            margin-bottom: 10px;
+          }
+        `}
       </style>
     </div>
   );
